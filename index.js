b593d853c53231aab1bf9c4d0763bead55fb663d
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-10-17 19:03:29 -0700

    Added Manager class that extends Employee

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+// require inquirer?
+
+
+
 class Employee {
-    constructor() {
+    constructor(name, id, email) {
         this.name = name;
         this.id = id;
         this.email = email;
     }
 }
 
+class Manager extends Employee {
+    constructor(officenumber) {
+        this.officenumber = officenumber;
+    }
+}
+
 class Engineer extends Employee {
-    constructor() {
+    constructor(github) {
         this.github = github;
     }
 }
 
 class Intern extends Employee {
-    constructor(){
+    constructor(school){
         this.school = school;
     }
 }
